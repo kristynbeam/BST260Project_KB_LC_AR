fef547288905bc83459d13cebd7a8eec9d9c4af9
Alexandra Rashedi <alexandra.rashedi@gmail.com>
2020-12-13 12:01:05 -0600

    ASR 12.13.20

--- Test3/app.R
+++ Test3/app.R
@@ -1,4 +1,4 @@
-
+# Bring in libraries
 library(shiny)
 library(tidyverse)
 library(dplyr)
@@ -9,19 +9,6 @@ library(directlabels)
 library(zoo)
 library(ozmaps)
 
-# Bring in data
-
-# We have to name a relative file path instead of an absolute one. It all depends on the directory we are working in. 
-# When they clone our github and run our code, they will be located in the "final_proj_app" to run the shinyapp
-# In order to get them into the correct folder to load the data files, we have to add the "../" before
-# the code in order to direct them to the correct folder.
-# As long as all of our data files are saved into the project_data_files folder, then this should work for the 
-# shiny app purposes.
-
-# aus_total_high = read.csv("../project_data_files/aus_total_high.csv")
-# pol_dat_aus = read.csv("../project_data_files/pol_dat_aus.csv")
-# aus_total_covid = read.csv("../project_data_files/aus_total_covid.csv")
-
 # Define UI for application 
 ui <- fluidPage(
     
@@ -29,73 +16,85 @@ ui <- fluidPage(
     theme = shinythemes::shinytheme("darkly"),
     
     # Application title
-    titlePanel("Trends in COVID-19 and Other Infectious Diseases in Australian States/Territories from 2019-2020"),
+    titlePanel("Trends in COVID-19 and Other Infectious Diseases in Australian States/Territories from 2018-2020"),
     
     fluidRow(
         
         sidebarLayout(
+            
             sidebarPanel(
+                
                 selectInput("state",
                             label = "Select a state/territory for plot:",
                             choices = unique(aus_total_high$State),
-                            selected = "Australian Capital Territory"), # end of state selection
+                            selected = "New South Wales"), # end of state/territory selection
+                
                 selectInput("disease",
                             label = "Select a disease for plot and map:",
                             choices = levels(factor(aus_total_high$Disease)),
-                            selected = "Hepatitis B (unspecified)"), # end of disease selection
+                            selected = "Influenza (laboratory confirmed)"), # end of disease selection
+                
                 selectInput("policy", 
                             label = "Select a policy restriction category for plot to display date of policy start (black line):",
                             choices = levels(factor(pol_dat_aus$type)),
                             selected = "Restrictions of Mass Gatherings"), # end of policy selection
+                
                 selectInput("date", 
                             label = "Select a date for plot (green line) and map:",
                             choices = aus_total_high$Date,
                             selected = "2019-01-01"), # end of date selection
+                
                 radioButtons(inputId="plot_type", 
                              label="Select a variable for plot and map:",
                              choices=c("New cases", 
                                        "New cases per million"),
-                             selected = "New cases") # end of y-axis selection
+                             selected = "New cases") # end of variable selection
                 
-            ), #end sidebarPanel
+                ), # end sidebarPanel
             
-            # Show a plot of the generated distribution
+            # Line plot
             mainPanel(
+                
                 plotOutput("linePlot")
-            ) #end main Panel
-        ),#end of fluid Row
+            
+                ) # end mainPanel
+        
+            ), #end sidebarLayout
         
         fluidRow(
-            #column(2,tableOutput("regression")),
-            column(3,tableOutput("table")), #end column
-                   
             
+            column(3,
+                   tableOutput("table")
+                   ), # end table column
+                   
             column(9,
                    plotOutput("mapPlot")
-            )#, #end column
+                   ) # end map column
             
-        ) #end fluidRow
+            ) # end fluidRow
         
-    )
-) # end UI
+        ) # end fluidRow
+    
+    ) # end fluidPage
 
-# Define server logic required to draw a histogram
+# Define server logic
 server <- function(input, output, session) {
     
-    updateSelectizeInput(session, 'Country_list', choices = aus_total_high$Disease, server = TRUE)
-    
+    # Line plot
     output$linePlot <- renderPlot({
         
-             if (input$plot_type=="New cases") {
+        # Count
+        if (input$plot_type == "New cases") {
             
             # Define diseases to include in line plot as COVID-19 and user-selected disease
             diseases <- c("COVID-19", 
                           input$disease)
             
-            # Define province and policy type
+            # Define state/territory and policy type
             pol_dat <- pol_dat_aus %>%
                 filter(province == input$state & type == input$policy)
             
+            # Output line plot
             aus_total_high %>% 
                 filter(State == input$state & Disease %in% diseases) %>% 
                 ggplot(aes(x = Date, 
@@ -105,25 +104,31 @@ server <- function(input, output, session) {
                 geom_vline(xintercept = as.numeric(as.Date(pol_dat$date_start)),
                            color = "black") +
                 geom_vline(xintercept = as.numeric(as.Date(input$date)),
-                           color = "green")+
+                           color = "green") +
                 theme_minimal() +
                 labs(x = "Date",
                      y = "New cases") +
                 ggtitle(paste("New confirmed cases over time in", 
                               input$state)) +
                 scale_x_date(date_breaks = "3 months", 
-                             date_labels = "%b %y")
+                             date_labels = "%b %y",
+                             limits = as.Date(c("2018-11-30", "2020-10-31"))) +
+                scale_y_continuous(trans = "log10",
+                                   limits = c(1, 32685)) +
+                scale_color_discrete(name = "Disease (log scale):")
             
-        } else if (input$plot_type=="New cases per million") {
+            # Rate
+            } else if (input$plot_type == "New cases per million") {
             
             # Define diseases to include in line plot as COVID-19 and user-selected disease
             diseases <- c("COVID-19", 
                           input$disease)
             
-            # Define province and policy type
+            # Define state/territory and policy type
             pol_dat <- pol_dat_aus %>%
                 filter(province == input$state & type == input$policy)
             
+            # Output line plot
             aus_total_high %>% 
                 filter(State == input$state & Disease %in% diseases) %>% 
                 ggplot(aes(x = Date, 
@@ -133,59 +138,54 @@ server <- function(input, output, session) {
                 geom_vline(xintercept = as.numeric(as.Date(pol_dat$date_start)),
                            color = "black") +
                 geom_vline(xintercept = as.numeric(as.Date(input$date)),
-                           color = "green")+
+                           color = "green") +
                 theme_minimal() +
                 labs(x = "Date",
                      y = "New cases per million") +
                 ggtitle(paste("New confirmed cases per million over time in", 
                               input$state)) +
                 scale_x_date(date_breaks = "3 months", 
-                             date_labels = "%b %y")
+                             date_labels = "%b %y",
+                             limits = as.Date(c("2018-11-30", "2020-10-31"))) +
+                scale_y_continuous(trans = "log10",
+                                   limits = c(0.2, 410)) +
+                scale_color_discrete(name = "Disease (log scale):")
             
-        }  
-    }) #end of plot
+            }
+        
+        }) # end of line plot
     
-    # output$regression <- renderPrint({
-    # 
-    #     aus_total_high <- aus_total_high %>%
-    #         filter(Disease == input$disease) %>%
-    #         subset(Date <= "2019-10-31") %>%
-    #         mutate(year1 = mean(Cases)) %>%
-    #         subset(Date >= "2019-10-31") %>%
-    #         group_by(State) %>%
-    #         mutate(year2 = mean(Cases)) %>%
-    # 
-    #         linearMod <- lm(aus_total_high$year1 ~ aus_total_high$year2)
-    #         confint.lm(linearMod)
-    #         summary(linearMod)
-    # 
-    # })#output of regression analysis
-
-    
+    # Table
     output$table <- renderTable({
-        Abbreviation <- c("ACT", "NSW", "NT", "Qld", "SA", "Tas", "Vic", "WA")
+        
+        Abbreviation <- c("ACT", 
+                          "NSW", 
+                          "NT", 
+                          "Qld", 
+                          "SA", 
+                          "Tas", 
+                          "Vic", 
+                          "WA")
+        
         Region <- c("Australian Capital Territory",
-                             "New South Wales",
-                             "Northern Territory",
-                             "Queensland",
-                             "Southern Territory",
-                             "Tasmania",
-                             "Victoria",
-                             "Western Australia")
+                    "New South Wales",
+                    "Northern Territory",
+                    "Queensland",
+                    "Southern Territory",
+                    "Tasmania",
+                    "Victoria",
+                    "Western Australia")
         table.df <- data.frame(Abbreviation, Region)
 
-    }) #end of table
-    
+        }) # end of table
     
     # Map plot
-    
-    # Need to make sure we write our final dataset as a csv to the folder and then read it in at the top
-    
     output$mapPlot <- renderPlot({
         
-       if (input$plot_type == "New cases") {
+        # Count
+        if (input$plot_type == "New cases") {
             
-           # Rename states
+           # Rename states/territories
            merge_dat$State <- recode(merge_dat$State, 
                                      "Australian Capital Territory" = "ACT", 
                                      "New South Wales" = "NSW", 
@@ -195,64 +195,72 @@ server <- function(input, output, session) {
                                      "Tasmania" = "Tas", 
                                      "Victoria" = "Vic", 
                                      "Western Australia" = "WA")
-           
+   
+            # Output map
             merge_dat %>% 
                 group_by(input$state) %>% 
                 filter(Disease == input$disease,
                        Date == input$date) %>% 
                 ggplot(aes(fill = Cases)) + 
-                geom_sf() +
+                geom_sf(colour="white") +
                 ggtitle(paste("New confirmed cases of", 
                               input$disease,
                               "on", 
                               input$date,
                               "by Australian state/territory")) +
-                scale_fill_viridis_c(name = "New cases (sqrt scale)",
-                                     trans = "sqrt",
-                                     direction = -1) +
-               geom_sf_label(aes(label = State),
-                             nudge_x = 3.5,
-                             nudge_y = -0.75) +
-                labs(x = "Latitude",
-                     y = "Longitude")
-            
-        } else if (input$plot_type == "New cases per million") {
-            
-            # Rename states
-            merge_dat$State <- recode(merge_dat$State, 
-                                      "Australian Capital Territory" = "ACT", 
-                                      "New South Wales" = "NSW", 
-                                      "Northern Territory" = "NT", 
-                                      "Queensland" = "Qld", 
-                                      "South Australia" = "SA", 
-                                      "Tasmania" = "Tas", 
-                                      "Victoria" = "Vic", 
-                                      "Western Australia" = "WA")
-            
-            merge_dat %>% 
-                group_by(input$state) %>% 
-                filter(Disease == input$disease,
-                       Date == input$date) %>% 
-                ggplot(aes(fill = Rates)) + 
-                geom_sf() +
-                ggtitle(paste("New confirmed cases of", 
-                              input$disease,
-                              "per million on", 
-                              input$date,
-                              "by Australian state/territory")) +
-                scale_fill_viridis_c(name = "New cases per mil (sqrt scale)",
-                                     trans = "sqrt",
-                                     direction = -1) +
+                scale_fill_viridis_c(name = "New cases (log scale)",
+                                     trans = "log10",
+                                     direction = -1,
+                                     limits = c(1, 32685),
+                                     na.value = "grey50") +
                 geom_sf_label(aes(label = State),
                               nudge_x = 3.5,
                               nudge_y = -0.75) +
                 labs(x = "Latitude",
                      y = "Longitude")
             
-        }  
-    }) #end of map plot
+            # Rate
+            } else if (input$plot_type == "New cases per million") {
+            
+                # Rename states/territories
+                merge_dat$State <- recode(merge_dat$State, 
+                                          "Australian Capital Territory" = "ACT", 
+                                          "New South Wales" = "NSW", 
+                                          "Northern Territory" = "NT", 
+                                          "Queensland" = "Qld", 
+                                          "South Australia" = "SA", 
+                                          "Tasmania" = "Tas", 
+                                          "Victoria" = "Vic", 
+                                          "Western Australia" = "WA")
+                
+                # Output map
+                merge_dat %>% 
+                    group_by(input$state) %>% 
+                    filter(Disease == input$disease,
+                           Date == input$date) %>% 
+                    ggplot(aes(fill = Rates)) + 
+                    geom_sf(colour="white") +
+                    ggtitle(paste("New confirmed cases of", 
+                                  input$disease,
+                                  "per million on", 
+                                  input$date,
+                                  "by Australian state/territory")) +
+                    scale_fill_viridis_c(name = "New cases per mil (log scale)",
+                                         trans = "log10",
+                                         direction = -1,
+                                         limits = c(0.2, 410),
+                                         na.value = "grey50") +
+                    geom_sf_label(aes(label = State),
+                                  nudge_x = 3.5,
+                                  nudge_y = -0.75) +
+                    labs(x = "Latitude",
+                         y = "Longitude")
+            
+            }  
+        
+        }) # end of map plot
     
-}
+    } # end of server
 
 # Run the application 
 shinyApp(ui = ui, server = server)
